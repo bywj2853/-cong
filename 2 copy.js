42e6a8751196bedba16db20f47caaeb272342bfc
教士 <111169491+bywj2853@users.noreply.github.com>
2022-09-15 12:38:17 +0800

    Add files via upload

--- 2 copy.js
+++ 2 copy.js
@@ -1,5 +1,6 @@
 var mySong = document.getElementById("mySong");
 var icon = document.getElementById("icon");
+var iconcopy = document.getElementById("iconcopy");
 
 icon.onclick = function () {
   if (mySong.paused) {
@@ -8,5 +9,14 @@ icon.onclick = function () {
   } else {
     mySong.pause();
     icon.src = "./图片文件(音乐)/play.png";
+  }
+};
+iconcopy.onclick = function () {
+  if (mySong.paused) {
+    mySong.play();
+    iconcopy.src = "./图片文件(音乐)/pause.png";
+  } else {
+    mySong.pause();
+    iconcopy.src = "./图片文件(音乐)/play.png";
   }
 };
