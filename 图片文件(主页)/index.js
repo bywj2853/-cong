cd9ee112e4fc253b233cc9481323a86639ae63d1
教士 <111169491+bywj2853@users.noreply.github.com>
2022-09-23 23:36:34 +0800

    Add files via upload

--- 图片文件(主页)/index.js
+++ 图片文件(主页)/index.js
@@ -1,13 +1,14 @@
-const body = document.querySelector("body");
-const sidebar = body.querySelector("nav");
-const toggle = body.querySelector(".toggle");
-const searchBtn = body.querySelector(".search-box");
-const modeSwitch = body.querySelector(".toggle-switch");
-const modeText = body.querySelector(".mode-text");
+const panels = document.querySelectorAll('.panel')
 
-toggle.addEventListener("click", () => {
-  sidebar.classList.toggle("close");
-});
-searchBtn.addEventListener("click", () => {
-  sidebar.classList.remove("close");
-});
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
